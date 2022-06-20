aec0a3eff06d7355afde991143567b89fdd3b75c
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.localdomain>
2022-06-20 14:23:49 -0500

    added seperate css file for code styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import Header from "../components/Header";
 import client from "../graphql-client";
+
 import "../styles/globals.css";
+import "../styles/code.css";
 
 import { ApolloProvider } from "@apollo/client";
 
