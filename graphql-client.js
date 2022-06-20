0774db61cf7be6141439ed95d5e62a5732c9ae56
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.localdomain>
2022-06-20 16:06:04 -0500

    test

--- graphql-client.js
+++ graphql-client.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-console.log(process.env.NODE_ENV, "process.env.NODE_ENV", process.env.backendAPIUrl);
+console.log(process.env.NODE_ENV, "process.env.NODE_ENV", process.env.BACKEND_API_URL);
 
 const client = new ApolloClient({
-  uri: process.env.backendAPIUrl || "https://squid-app-7h5w9.ondigitalocean.app/graphql",
+  uri: process.env.BACKEND_API_URL || "https://squid-app-7h5w9.ondigitalocean.app/graphql",
   cache: new InMemoryCache(),
 });
 
