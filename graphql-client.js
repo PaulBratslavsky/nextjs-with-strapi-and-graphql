798acd225b869b1bb4da36c47bcf0c2457e39049
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.localdomain>
2022-06-20 15:50:34 -0500

    heh

--- graphql-client.js
+++ graphql-client.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 console.log(process.env.NODE_ENV, "process.env.NODE_ENV", process.env.backendAPIUrl);
+
 const client = new ApolloClient({
-  uri: process.env.backendAPIUrl || "http://localhost:1337/graphql",
+  uri: process.env.backendAPIUrl || "https://squid-app-7h5w9.ondigitalocean.app/graphql",
   cache: new InMemoryCache(),
 });
 
