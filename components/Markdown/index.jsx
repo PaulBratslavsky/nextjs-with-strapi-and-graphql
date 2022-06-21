a95ae40aa16bb535d18c2ef20756289d72620241
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.localdomain>
2022-06-20 20:08:27 -0500

    style markdown

--- components/Markdown/index.jsx
+++ components/Markdown/index.jsx
@@ -11,8 +11,11 @@ export default function Markdown({ data }) {
 
 
   const components = {
-    h1: (props) => <h1 className="my-3 text-slate-800 text-4xl	">{props.children}</h1>,
-    h2: (props) => <h1 className="my-1 text-slate-800 text-2xl">{props.children}</h1>,
+    h1: (props) => <h1 className="my-6 text-secondary font-bold text-4xl	">{props.children}</h1>,
+    h2: (props) => <h1 className="mt-6 mb-3 text-secondary text-3xl">{props.children}</h1>,
+    p: (props) => <p className="leading-relaxed my-6 mb-3 text-slate-700 text-xl ">{props.children}</p>,
+    ul: (props) => <ul className="pl-5 my-3 ">{props.children}</ul>,
+    li: (props) => <li className="my-1 list-disc text-lg text-slate-700">{props.children}</li>,
     pre: (props) => <pre className="p-6">{props.children}</pre>,
 
   };
